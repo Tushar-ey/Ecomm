53957f8f62bd7b2880bf789e95ca185257553f21
Tushar EY <Tusha3@gds.ey.com>
2025-01-18 13:03:07 +0530

    Changes in ControllerTest

--- inventory-service/src/test/java/com/techie/microservices/inventory/controller/InventoryControllerTest.java
+++ inventory-service/src/test/java/com/techie/microservices/inventory/controller/InventoryControllerTest.java
@@ -1,4 +1,18 @@
+package com.techie.microservices.inventory.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(InventoryController.class)
 class InventoryControllerTest {
-  
+
+    @Test
+    public void isInStock() {
+
+        System.out.println("test");
+
+    }
+
 }
